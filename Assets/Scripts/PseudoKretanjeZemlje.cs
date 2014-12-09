ac24bc66b78bbd910edbb498d784ef4f1d0a1c36
LazarRankovic <lazar.rankovic.94@gmail.com>
2014-12-09 02:24:44 +0100

    Sunce_kretanje_oko_sunca
    Kvadrat

---
+++ Assets/Scripts/PseudoKretanjeZemlje.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PseudoKretanjeZemlje : MonoBehaviour 
+{
+    int x_pomeraj=-15, z_pomeraj=15;
+	// Use this for initialization
+	
+	// Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x < -1400)
+        {
+            x_pomeraj = +15;
+        }
+        if (transform.position.x > 1400)
+        {
+            x_pomeraj = -15;
+        }
+        if(transform.position.z < -1400)
+        {
+            z_pomeraj = +15;
+        }
+        if (transform.position.z > 1400)
+        {
+            z_pomeraj = -15;
+        }
+        transform.Translate(new Vector3(x_pomeraj, 0, z_pomeraj));
+    }	
+}
